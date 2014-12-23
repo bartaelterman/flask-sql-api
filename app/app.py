0115a19e0cc00b0292dce0453f404c8bf1a076bb
Bart Aelterman <bart.aelterman@gmail.com>
2014-12-23 13:16:46 +0100

    Add simple sql check

--- app/app.py
+++ app/app.py
@@ -27,14 +27,25 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+def query_is_ok(sql):
+    qok = True
+    if sql[0:6] != 'select':
+        qok = False
+    if ';' in sql:
+        qok = False
+    return qok
+
 # This is my custom method that will accept a `sql` parameter
 # to the HTTP GET request, and executs the query
 @app.route('/')
 def show_entries():
     sql = request.args.get('sql', 'select * from nbastats')
-    cur = g.db.execute(sql)
-    entries = cur.fetchall()
-    return json.dumps(entries)
+    if query_is_ok(sql):
+        cur = g.db.execute(sql)
+        entries = cur.fetchall()
+        return json.dumps(entries)
+    else:
+        return json.dumps({'error': 'you\'re query is considered to be insecure'})
 
 # This function will run the app
 if __name__ == '__main__':
